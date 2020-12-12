2ad7ef6e4602027a06bf2d65e420fcffa9c3bc74
yunhaeyoung <haeyoung102@gmail.com>
2020-12-12 21:22:27 +0900

    bee image

--- app/src/main/java/peanutbutterfam/catchfly/MainActivity.java
+++ app/src/main/java/peanutbutterfam/catchfly/MainActivity.java
@@ -3,6 +3,7 @@ package peanutbutterfam.catchfly;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button pos00 = (Button)findViewById(R.id.pos00);
+
+        pos00.setBackgroundResource(R.mipmap.bee);
     }
 }
